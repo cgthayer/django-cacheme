c49e677a35f24d891e3bad910006683890d8abf6
Yiling-J <njjyl723@gmail.com>
2019-05-31 11:04:05 +0800

    update admin

--- django_cacheme/models.py
+++ django_cacheme/models.py
@@ -1,12 +1,16 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-from .utils import invalid_pattern
+from .utils import invalid_pattern, CACHEME
+
+
+def default_pattern():
+    return CACHEME.REDIS_CACHE_PREFIX + ':'
 
 
 class Invalidation(models.Model):
     user = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
-    pattern = models.CharField(max_length=200)
+    pattern = models.CharField(max_length=200, default=default_pattern)
     created = models.DateTimeField(default=timezone.now)
 
     def __str__(self):
