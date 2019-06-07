30d8ecc5a50cb27fb2ea5787cc0917b8b269b5c8
Yiling-J <njjyl723@gmail.com>
2019-06-07 17:38:16 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,40 @@
+from setuptools import setup
+
+
+description = 'Django-Cacheme is a memoized decorator for Django using redis'
+
+
+setup(
+    name="django-cacheme",
+    url="https://github.com/Yiling-J/django-cacheme",
+    author="Yiling",
+    author_email="njjyl723@gmail.com",
+    version='v0.0.1',
+    packages=[
+        "django_cacheme",
+    ],
+    description=description,
+    python_requires=">=3.5",
+    install_requires=[
+        "django_redis>=4.10.0",
+    ],
+    zip_safe=False,
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Framework :: Django :: 1.11",
+        "Framework :: Django :: 2.0",
+        "Framework :: Django :: 2.1",
+        "Framework :: Django :: 2.2",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: BSD License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.5",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Topic :: Software Development :: Libraries",
+        "Topic :: Utilities",
+    ],
+)
