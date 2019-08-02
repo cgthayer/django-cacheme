f4027ceed7928e739a6959c6ca1c842be3130a69
Yiling-J <njjyl723@gmail.com>
2019-08-02 23:14:19 +0800

    update setup version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     author="Yiling",
     author_email="njjyl723@gmail.com",
     license="BSD-3-Clause",
-    version='v0.0.7',
+    version='v0.0.8',
     packages=[
         "django_cacheme",
     ],
